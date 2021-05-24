73893f9e88accbce61204585cc8fa112f0a921ac
VladiKruk <vladi-kruk@gmx.de>
2021-05-24 16:13:14 +0200

    Clean up file

--- ecg_classification/baseline.py
+++ ecg_classification/baseline.py
@@ -1,13 +1,8 @@
 from wettbewerb import load_references
 import pandas as pd
-import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import plot_confusion_matrix
 
 # Load dataset with list of leads (numpy arr), labels (str), fs and ecg names
 ecg_leads, ecg_labels, fs, ecg_names = load_references(folder="/Users/Vladi/Desktop/training/")
@@ -27,7 +22,6 @@ label_encoder = preprocessing.LabelEncoder()
 label_encoder = label_encoder.fit(ecg_labels)
 ecg_labels = label_encoder.transform(ecg_labels)
 
-print(ecg_labels[5990:])
 
 # Split data into training and validation set
 X_train, X_test, y_train, y_test = train_test_split(ecg_leads, ecg_labels, test_size=0.2, random_state=42)
