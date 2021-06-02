6108b738d2a3700b3c746f89c13aed9b84ab8367
VladiKruk <vladi-kruk@gmx.de>
2021-06-02 12:58:06 +0200

    Added F1 Score

--- ecg_classification/baseline.py
+++ ecg_classification/baseline.py
@@ -4,6 +4,8 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import pickle
 
 # Load dataset with list of leads (numpy arr), labels (str), fs and ecg names
 ecg_leads, ecg_labels, fs, ecg_names = load_references(folder="/Users/Vladi/Desktop/training/")
@@ -55,6 +57,7 @@ dtest = xgb.DMatrix(X_test, label=y_test)
 # Specify params for xgboost
 param = {'max_depth': 10, 'eta': 0.1, 'gamma': 0, 'lambda': 1, 'objective': 'multi:softprob', 'num_classes': 4}
 
+
 # Init classifier and fit model
 classifier_xgb = xgb.XGBClassifier(param)
 model = classifier_xgb.fit(X_train,
@@ -65,9 +68,16 @@ model = classifier_xgb.fit(X_train,
                            eval_set=[(X_test, y_test)]
 )
 
+
 # Save result evaluation
 eval_result = model.evals_result()
 
+'''
+# Init saved model
+loaded_model = xgb.Booster()
+# Load data
+loaded_model.load_model('baseline_model')
+'''
 train_pred = model.predict(X_train)
 print('\nTarget on train data', train_pred)
 
@@ -75,13 +85,27 @@ print('\nTarget on train data', train_pred)
 train_accuracy = accuracy_score(y_train, train_pred)
 print('\naccuracy_score on train dataset : ', train_accuracy)
 
+'''
+F1 Score calculated as:
+F1 = 2 * (precision * recall) / (precision + recall)
+'''
+
+# Compute F1 Score on Training Set
+f1_train = f1_score(y_train, train_pred, average='macro')
+print('\nF1 Score on training dataset : ', f1_train)
+
 # predict the target on the test dataset
-predict_test = model.predict(X_test)
-print('\nTarget on test data', predict_test)
+test_pred = model.predict(X_test)
+print('\nTarget on test data', test_pred)
 
 # Accuracy Score on test dataset
-accuracy_test = accuracy_score(y_test, predict_test)
-print('\naccuracy_score on test dataset : ', accuracy_test)
+test_accuracy = accuracy_score(y_test, test_pred)
+print('\naccuracy_score on test dataset : ', test_accuracy)
+
+# Compute F1 Score on Test Set
+f1_test = f1_score(y_test, test_pred, average='macro')
+print('\nF1 Score on test dataset : ', f1_test)
 
 #model.save_model("baseline_model")
+
 
